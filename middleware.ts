8a57dec8cba70dddef623e5fc3937fa8f20cb1d6
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-03-12 20:45:16 +0530

    middleware added

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get('token')?.value;
+  const requestHeaders = new Headers(req.headers);
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  requestHeaders.set('Authorization', `Bearer ${token}`);
+
+  return NextResponse.next({
+    headers: requestHeaders,
+  });
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+};
